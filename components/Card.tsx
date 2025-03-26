92da9ec4472c8f29c42c8b9ca321239dbd0a01f6
pongangairaman-nks <pongangairamannks@gmail.com>
2025-03-26 10:51:29 +0530

    Adding a bug fix in Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,36 +1,41 @@
 import React from "react";
 import Image from "next/image";
 import { useDispatch } from "react-redux";
-import{setShowDialog, setClickedcard, fetchVideoDescription} from "../redux/slices/movieSlice"
+import {
+  setShowDialog,
+  setClickedcard,
+  fetchVideoDescription
+} from "../redux/slices/movieSlice";
 import { Video } from "./CardSlider";
 
 interface CardProps {
   imageUrl: string;
   title: string;
   videoUrl: string;
-  ele:Video
+  ele: Video;
 }
 
-const Card: React.FC<CardProps> = ({ imageUrl, title, videoUrl,ele }) => {
-  const dispatch = useDispatch()
+const Card: React.FC<CardProps> = ({ imageUrl, title, videoUrl, ele }) => {
+  const dispatch = useDispatch();
   // const [showDialog, setShowDIalog] = useState<boolean>(false);
 
-  const hanldeVideo = async (video: string, image: string,ele:Video) => {
-    console.log("clicked")
-    let res={}
-    if(!ele?.aiDescription){
-     res = await dispatch(fetchVideoDescription(ele?._id))
+  const hanldeVideo = async (video: string, image: string, ele: Video) => {
+    console.log("clicked");
+    if (!ele?.aiDescription) {
+      dispatch(fetchVideoDescription(ele?._id));
     }
     // if(res){
-      dispatch(setShowDialog(true))
-      dispatch(setClickedcard(ele))
+    dispatch(setShowDialog(true));
+    dispatch(setClickedcard(ele));
     // }
- 
   };
 
   return (
     <>
-      <div className="card" onClick={() => hanldeVideo(videoUrl, imageUrl,ele)}>
+      <div
+        className="card"
+        onClick={() => hanldeVideo(videoUrl, imageUrl, ele)}
+      >
         <div className="card-image-container">
           <Image
             src={imageUrl}
@@ -47,8 +52,6 @@ const Card: React.FC<CardProps> = ({ imageUrl, title, videoUrl,ele }) => {
         <p className="card-description">{description}</p>
       </div> */}
       </div>
-
-   
     </>
   );
 };
